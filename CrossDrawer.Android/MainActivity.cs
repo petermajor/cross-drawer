95f75452d6e520d2c6747d41b399d7f7043b80c6
Peter Major <peter@petermajor.co.uk>
2016-02-25 14:25:03 +0000

    Toolbar shows over app drawer

---
+++ CrossDrawer.Android/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace CrossDrawer.Android
+{
+	[Activity (MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
+	{
+		Fragment[] _fragments = { new MyListFragment(), new MySettingsFragment() };
+
+		string[] _titles = { "My List", "My Settings" };
+
+		ActionBarDrawerToggle _drawerToggle;
+
+		ListView _drawerListView;
+
+		DrawerLayout _drawerLayout;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			SetContentView (Resource.Layout.Main);
+
+			SupportActionBar.SetDisplayHomeAsUpEnabled (true);
+
+			_drawerListView = FindViewById<ListView> (Resource.Id.drawerListView);
+			_drawerListView.ItemClick += (s, e) => ShowFragmentAt (e.Position);
+			_drawerListView.Adapter = new ArrayAdapter<string> (this, global::Android.Resource.Layout.SimpleListItem1, _titles);
+
+			_drawerLayout = FindViewById<DrawerLayout> (Resource.Id.drawerLayout);
+
+			_drawerToggle = new ActionBarDrawerToggle (this, _drawerLayout, Resource.String.OpenDrawerString, Resource.String.CloseDrawerString);
+
+			_drawerLayout.SetDrawerListener (_drawerToggle);
+
+			ShowFragmentAt (0);
+		}
+
+		void ShowFragmentAt (int position)
+		{
+			SupportFragmentManager.BeginTransaction ().Replace (Resource.Id.frameLayout, _fragments [position]).Commit ();
+
+			Title = _titles [position];
+
+			_drawerLayout.CloseDrawer (_drawerListView);
+		}
+
+		protected override void OnPostCreate (Bundle savedInstanceState)
+		{
+			_drawerToggle.SyncState ();
+
+			base.OnPostCreate (savedInstanceState);
+		}
+
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{
+			if (_drawerToggle.OnOptionsItemSelected (item))
+				return true;
+
+			return base.OnOptionsItemSelected (item);
+		}
+	}
+}
---
+++ CrossDrawer.Android/MySettingsFragment.cs
@@ -0,0 +1,14 @@
+﻿using Android.OS;
+using Android.Views;
+using Fragment = Android.Support.V4.App.Fragment;
+
+namespace CrossDrawer.Android
+{
+	public class MySettingsFragment : Fragment
+	{
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			return inflater.Inflate(Resource.Layout.MySettingsView, container, false);
+		}
+	}
+}
